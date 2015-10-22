28dbb186a6172504ce1a9d0fd4a3cd78082c4aa0
李玄武 <410230697@qq.com>
2015-10-22 17:17:29 +0800

    first commit

---
+++ src/main/java/com/wonders/lxw/project/App.java
@@ -0,0 +1,13 @@
+package com.wonders.lxw.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
